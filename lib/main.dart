10a3ad473108990debd861320bb2153f13cd5572
Sarath Krishnan <pvsarathkrishnan@gmail.com>
2024-04-13 00:50:03 +0530

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:knack_admin/presentation/controller_screen.dart';
-import 'package:knack_admin/presentation/home_screen.dart';
-import 'package:knack_admin/presentation/login_screen.dart';
+import 'package:knack_admin/firebase_options.dart';
 import 'package:knack_admin/presentation/splash_screen.dart';
 import 'package:knack_admin/presentation/user_management.dart';
 
 
 const SAVE_KEY_NAME ="UserLoggedIn";
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -19,11 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      // home: SplashScreen(screenWidth: screenWidth));
-      home: UserDetailsScreen(),
-      // home: SplashScreen(
-      //   screenWidth: screenWidth,
-      // ),
+    home: SplashScreen(),
     );
   }
 }
--- lib/presentation/user_management.dart
+++ lib/presentation/user_management.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:knack_admin/presentation/style/text_style.dart';
 
 class UserDetailsScreen extends StatefulWidget {
@@ -14,7 +14,10 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User Details",style: t1,),
+        title: Text(
+          "User Details",
+          style: t1,
+        ),
         backgroundColor: Color.fromARGB(255, 9, 220, 62).withOpacity(0.5),
         centerTitle: true,
       ),
@@ -22,35 +25,47 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
         child: Container(
           width: double.infinity,
           decoration: BoxDecoration(
-          
             boxShadow: [
               BoxShadow(
                 color: Color.fromARGB(255, 233, 233, 233).withOpacity(0.5),
-               
               ),
             ],
           ),
           child: SingleChildScrollView(
-            child: DataTable(
-              headingRowHeight: 60,
-              columns: [
-                DataColumn(label: Text('sl/no', style: t1)),
-                DataColumn(label: Text('Name', style: t1)),
-                DataColumn(label: Text('Email', style: t1)),
-                DataColumn(label: Text('Course Selected', style: t1)),
-                DataColumn(label: Text('Premium Course', style: t1)),
-              ],
-              rows: List.generate(
-                20,
-                (index) => DataRow(cells: [
-                  DataCell(Text((index + 1).toString(), style: t1)),
-                  DataCell(Text('Sarath Krishnan', style: t1)),
-                  DataCell(Text('gmail@gmail.com', style: t1)),
-                  DataCell(
-                      Text('0', style: t1)), // Assuming 0 means not selected
-                  DataCell(Text('2', style: t1)), // Assuming 2 means selected
-                ]),
-              ),
+            child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              future: FirebaseFirestore.instance.collection('users').get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return DataTable(
+                    headingRowHeight: 60,
+                    columns: [
+                      DataColumn(label: Text('sl/no', style: t1)),
+                      DataColumn(label: Text('Name', style: t1)),
+                      DataColumn(label: Text('Email', style: t1)),
+                      DataColumn(label: Text('Course Selected', style: t1)),
+                      DataColumn(label: Text('Premium Course', style: t1)),
+                    ],
+                    rows: snapshot.data!.docs.map(
+                      (document) {
+                        int rowIndex = snapshot.data!.docs.indexOf(document) + 1;
+                        Map<String, dynamic> data = document.data()!;
+                        return DataRow(cells: [
+                          DataCell(Text(rowIndex.toString(), style: t1)), // sl/no starts from 1
+                          DataCell(Text(data['name'].toString(), style: t1)),
+                          DataCell(Text(data['email'].toString(), style: t1)),
+                          DataCell(Text(data['course_selected'].toString(), style: t1)),
+                          DataCell(Text(data['premium_course'].toString(), style: t1)),
+                        ]);
+                      },
+                    ).toList(),
+                  );
+                }
+              },
             ),
           ),
         ),
